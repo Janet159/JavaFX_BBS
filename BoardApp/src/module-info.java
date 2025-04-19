c92b6036ab192f7cf518ea2ddc32bb5695f8d6b8
Janet159 <janet15916@gmail.com>
2025-04-19 23:27:10 +0900

    b

--- BoardApp/src/module-info.java
+++ BoardApp/src/module-info.java
@@ -1,7 +1,10 @@
 module BoardApp {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires lombok;
+	requires java.sql;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml, lombok, java.sql;
 	opens application.Controller to javafx.fxml;
+	opens application.DTO to javafx.base;
 }
